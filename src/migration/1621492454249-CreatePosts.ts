657bb0f752a2b6d4fc620a5d52849f060d1e1ae8
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-20 16:43:40 +0800

    post entity;update readme

---
+++ src/migration/1621492454249-CreatePosts.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+export class CreatePosts1621492454249 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'posts',
+            columns: [
+                {name: 'id', type: 'int', isPrimary: true, isGenerated: true, generationStrategy: 'increment'},
+                {name: 'title', type: 'varchar'},
+                {name: 'content', type: 'text'}
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('posts');
+    }
+
+}
