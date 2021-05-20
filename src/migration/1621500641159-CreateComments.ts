db9a5a93074f55a1bc310d9442a87efca719054a
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-20 17:01:11 +0800

    remove dist;migration create

---
+++ src/migration/1621500641159-CreateComments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateComments1621500641159 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
