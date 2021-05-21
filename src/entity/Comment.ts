8953e2024b3ecdc61c94e23d643cc7e0d1a38731
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-21 14:56:33 +0800

    创建实体

---
+++ src/entity/Comment.ts
@@ -0,0 +1,24 @@
+import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
+import {User} from './User';
+import {Post} from './Post';
+
+@Entity('comments')
+export class Comment {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @ManyToOne(() => User, user => user.comments)
+    user: User;
+
+    @ManyToOne(() => Post, post => post.comments)
+    post: Post;
+
+    @Column('text')
+    content: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
