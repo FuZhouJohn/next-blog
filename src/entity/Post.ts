68ec03a14a04702cb19c80fc7cd9a9999ec43f95
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-21 14:56:38 +0800

    创建实体

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,15 +1,34 @@
-import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne, OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm';
+import {User} from './User';
+import {Comment} from './Comment';
 
 @Entity('posts')
 export class Post {
     @PrimaryGeneratedColumn('increment')
     id: number;
+
+    @ManyToOne(() => User, user => user.posts)
+    author: User;
+
     @Column('varchar')
     title: string;
+
     @Column('text')
     content: string;
 
-    constructor(attributes: Partial<Post>) {
-        Object.assign(this, attributes);
-    }
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @OneToMany(() => Comment, comment => comment.post)
+    comments: Comment[];
 }
