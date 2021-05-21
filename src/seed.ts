e4ed48aed589fe59c12898dafffa36816ee155df
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-21 16:04:21 +0800

    更新 seed 脚本

--- src/seed.ts
+++ src/seed.ts
@@ -1,15 +1,27 @@
 import 'reflect-metadata';
 import {createConnection} from 'typeorm';
 import {User} from './entity/User';
-
+import {Post} from './entity/Post';
+import {Comment} from './entity/Comment';
 createConnection().then(async connection => {
     const {manager} = connection;
 
-    const u1 = new User()
-    u1.username = 'zhuang'
-    u1.passwordDigest = 'xxx'
-    await manager.save(u1)
-    console.log(u1.id)
+    const user = new User()
+    user.username = 'zhuang5'
+    user.passwordDigest = 'xxx'
+    await manager.save(user)
+
+    const post = new Post()
+    post.title='这是一个标题4'
+    post.content='随便写点啥4'
+    post.author = user
+    await manager.save(post)
+
+    const comment = new Comment()
+    comment.content = '键盘侠启动'
+    comment.post = post
+    comment.user = user
+    await manager.save(comment)
 
     await connection.close()
 }).catch(error => console.log(error));
