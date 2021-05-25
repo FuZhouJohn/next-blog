ca3f43a1ae34bdef47a6ec98449c79af5d2ceb9f
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-25 17:13:24 +0800

    使用环境变量文件保存密钥

--- lib/withSession.ts
+++ lib/withSession.ts
@@ -1,13 +1,12 @@
-import {NextApiHandler} from 'next';
+import {GetServerSideProps, NextApiHandler} from 'next';
 import {withIronSession} from 'next-iron-session';
 
-export function withSession(handler: NextApiHandler) {
+export function withSession(handler: NextApiHandler | GetServerSideProps) {
     return withIronSession(handler, {
-        // password: process.env.SECRET_COOKIE_PASSWORD,
-        password: '1f9a7c08-db78-4a30-bb2c-d8ae2a87686c',
+        password: process.env.SECRET,
         cookieName: 'blog',
-        cookieOptions:{
-            secure:false
+        cookieOptions: {
+            secure: false
         }
     });
 }
