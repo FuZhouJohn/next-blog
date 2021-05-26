83d00d8d3f89e0f4b25e2887ed92b0fa33727046
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-26 15:15:47 +0800

    封装 Form.tsx

---
+++ components/Form.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEventHandler, FormEventHandler, ReactChild} from 'react';
+
+type Props = {
+    fields: {
+        label: string,
+        type: 'text' | 'password',
+        value: string | number,
+        onChange: ChangeEventHandler<HTMLInputElement>,
+        errors: string[]
+    }[]
+    onSubmit: FormEventHandler<HTMLFormElement>,
+    buttons: ReactChild
+}
+
+export const Form: React.FC<Props> = (props) => {
+    return (
+        <form onSubmit={props.onSubmit}>
+            {
+                props.fields.map(field => <div key={field.label}>
+                    <label>{field.label}
+                        <input type={field.type} value={field.value} onChange={field.onChange}/>
+                    </label>
+                    {field.errors?.length > 0 && <div>
+                        {field.errors.join(',')}
+                    </div>}
+                </div>)
+            }
+            <div>
+                {props.buttons}
+            </div>
+        </form>
+    );
+};
+
