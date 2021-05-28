fccfccea3de061847ecf88bd4650e73ec23ee8d8
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-28 16:39:14 +0800

    update orm config

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,26 +1,27 @@
-import 'reflect-metadata';
-import {createConnection, getConnectionManager} from 'typeorm';
-import {Post} from 'src/entity/Post';
-import {User} from 'src/entity/User';
-import {Comment} from 'src/entity/Comment';
-import config from 'ormconfig.json';
+import "reflect-metadata";
+import { createConnection, getConnectionManager } from "typeorm";
+import { Post } from "src/entity/Post";
+import { User } from "src/entity/User";
+import { Comment } from "src/entity/Comment";
+import config from "ormconfig.json";
 
 const create = async () => {
-    // @ts-ignore
-    return createConnection({
-        ...config,
-        entities: [Post, User, Comment]
-    });
+  // @ts-ignore
+  return createConnection({
+    ...config,
+    port: process.env.NODE_ENV === "production" ? 5433 : config.port,
+    entities: [Post, User, Comment],
+  });
 };
 const promise = (async function () {
-    const manager = getConnectionManager();
-    const current = manager.has('default') && manager.get('default');
-    if (current) {
-        await current.close();
-    }
-    return create();
+  const manager = getConnectionManager();
+  const current = manager.has("default") && manager.get("default");
+  if (current) {
+    await current.close();
+  }
+  return create();
 })();
 
 export const getDatabaseConnection = async () => {
-    return promise;
+  return promise;
 };
