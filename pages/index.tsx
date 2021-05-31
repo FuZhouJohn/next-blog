dfe7cbdf630443d7c9347bc2dbc1f4398cafbd00
zhuangjinan <zhuangjinan2018@gmail.com>
2021-05-31 17:41:51 +0800

    首页 css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,32 @@
-import PostsIndex, { getServerSideProps } from "./posts";
+import { NextPage } from "next";
+import Link from "next/link";
+import styled from "styled-components";
 
-export default PostsIndex;
-export { getServerSideProps };
+const Cover = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  img {
+    width: 300px;
+    height: auto;
+  }
+`;
+
+const Home: NextPage = () => {
+  return (
+    <Cover>
+      <img src="/logo.png" alt="" />
+      <h1>测试博客</h1>
+      <p>装逼的文字</p>
+      <p>
+        <Link href="/posts">
+          <a>文章列表</a>
+        </Link>
+      </p>
+    </Cover>
+  );
+};
+
+export default Home;
