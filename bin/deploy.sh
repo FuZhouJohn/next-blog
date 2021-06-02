226c9390105097df04a20673428632640c055c6f
zhuangjinan <zhuangjinan2018@gmail.com>
2021-06-02 11:24:05 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,9 @@
-echo Hello > /tmp/frank
+docker start 1dc5ef2e225f &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker kill blog-app
+docker build . -t zhuang/node-web-app &&
+docker run --name blog-app --network host -d zhuang/node-web-app &&
+echo 'OK!'
