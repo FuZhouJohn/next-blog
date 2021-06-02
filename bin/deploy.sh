250f1de78dc521078180ac5803aedb6bf2e6ae5a
zhuangjinan <zhuangjinan2018@gmail.com>
2021-06-02 11:30:29 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,8 @@ cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
 yarn build &&
-docker kill blog-app
+docker kill blog-app &&
+docker rm blog-app &&
 docker build . -t zhuang/node-web-app &&
 docker run --name blog-app --network host -d zhuang/node-web-app &&
 echo 'OK!'
